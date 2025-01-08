93b5839c26c50b7c94753b1569f9dea7cf97c159
faizan-habib839 <faizanhabib599@gmail.com>
2025-01-08 10:07:04 +0500

    new changes

--- .devcontainer/basic/project/weatherapp.js
+++ .devcontainer/basic/project/weatherapp.js
@@ -1,31 +1,77 @@
 const apiKey="6c287d610b97754447a44bed49dc749f";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=matric&q=";
 
 
-const searchbar=document.querySelector(".search input");
-const searchbutton=document.querySelector(".search button");
+const searchbox=document.querySelector(".search input");
+ const searchbtn=document.querySelector(".search button");
+ const weathericon=document.querySelector(".weather-icon");
 
-async function checkApi(city) {
-    let response = await fetch(apiUrl + `&appid=${apiKey}`);
-    let data = await response.json();
-    console.log(data);
+
+
+  async function checkwaether(city) {
+    const response =await fetch(apiUrl + city + `&appid=${apiKey}`);
+if(response.status ==404)
+{
+    document.querySelector(".error").style.display="block";
+    document.querySelector(".weather").style.display="none";
+}
+else
+{
+    var data=await response.json();
+    
+
 
     document.querySelector(".temp").innerHTML=Math.round(data.main.temp) + "°C" ;
     document.querySelector(".city").innerHTML=data.name;
     document.querySelector(".Humidity").innerHTML=data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " " + "km/h";
 
+    if(data.weather[0].main=="Clouds"){
+        weathericon.src="weather-app-img/images/clouds.png"
+
+    }
+    else if(data.weather[0].main=="Clear"){
+        weathericon.src="weather-app-img/images/clear.png"
+
+    }
+
+    else if(data.weather[0].main=="Clear"){
+        weathericon.src="weather-app-img/images/clear.png"
+
+    }
+
+    else if(data.weather[0].main=="Rain"){
+        weathericon.src="weather-app-img/images/rain.png"
+
+    }
+    else if(data.weather[0].main=="Drazzle"){
+        weathericon.src="weather-app-img/images/drizzle.png"
+
+    }
+
+    else if(data.weather[0].main=="Mist"){
+        weathericon.src="weather-app-img/images/mist.png"
+
+    }
+    document.querySelector(".weather").style.display="block";
+    document.querySelector(".error").style.display="none";
+
     
+  }
+}
+
    
+  searchbtn.addEventListener("click", ()=>{
 
-    searchbutton.addEventListener("click", () => {
-        checkApi(searchbar.value);
-    });
-    
-    
-    
-    
+    checkwaether(searchbox.value);
+  })
+ 
 
-}  
+
+     
+
+  
+
+
 
 
